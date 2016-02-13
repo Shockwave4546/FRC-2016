78ed16be692ae5ca400d114954c079796f663766
ShockwaveComputer <dovershockwave@gmail.com>
2016-02-13 10:30:22 -0500

    Finished replacing potentiometer

--- Dave/src/org/usfirst/frc/team4546/robot/commands/AimToFire.java
+++ Dave/src/org/usfirst/frc/team4546/robot/commands/AimToFire.java
@@ -1,12 +1,13 @@
 package org.usfirst.frc.team4546.robot.commands;
 
+import org.usfirst.frc.team4546.robot.Robot;
 import org.usfirst.frc.team4546.robot.RobotMap;
 
 import edu.wpi.first.wpilibj.command.Command;
 
 public class AimToFire extends Command {
 	
-	double theta = RobotMap.cannonPotentiometer.get();
+	double theta = Robot.cannon.getPitch();
 	
 	double cannonHeight;	//Physically Measured
 	double PMHeight;		//Physically Measured
