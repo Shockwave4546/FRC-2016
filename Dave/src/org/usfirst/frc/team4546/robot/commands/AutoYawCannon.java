9ce1ee0687fdad004e9632f57b4658b36b2adf55
craftgarrick04401 <craftgarrick04401@gmail.com>
2016-02-20 17:30:00 -0500

    Added more autonomous control commands

---
+++ Dave/src/org/usfirst/frc/team4546/robot/commands/AutoYawCannon.java
@@ -0,0 +1,39 @@
+package org.usfirst.frc.team4546.robot.commands;
+
+import org.usfirst.frc.team4546.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class AutoYawCannon extends Command {
+
+	double speed;
+	double angle;
+	
+	public AutoYawCannon(double speed, double angle)	{
+		
+		this.speed = speed;
+		this.angle = angle;
+	}
+	
+	protected void initialize() {
+
+	}
+
+	protected void execute() {
+
+		Robot.cannon.setYawMotor(speed, 1);
+	}
+	
+	protected boolean isFinished() {
+		return Robot.cannon.getYaw() == angle;
+	}
+
+	protected void end() {
+
+	}
+
+	protected void interrupted() {
+
+	}
+
+}
