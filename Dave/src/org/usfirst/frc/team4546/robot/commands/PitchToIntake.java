d352e66631cbd77437e9929c72b2b338c67aeb6a
Nolan Greig <ngreig85@comcast.net>
2016-02-22 17:19:49 -0500

    SLight change

--- Dave/src/org/usfirst/frc/team4546/robot/commands/PitchToIntake.java
+++ Dave/src/org/usfirst/frc/team4546/robot/commands/PitchToIntake.java
@@ -1,12 +1,14 @@
 package org.usfirst.frc.team4546.robot.commands;
 
+import org.usfirst.frc.team4546.robot.Robot;
+
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class PitchToIntake extends CommandGroup {
 	
 	public PitchToIntake()	{
 		
-		addSequential(new AutoPitchCannon(1, 0));
+		addSequential(new AutoPitchCannon(-1, Robot.lowerLimit));
 	}
 
 }
