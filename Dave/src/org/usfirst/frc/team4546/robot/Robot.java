b2e821941f45b5a19b714414bdf3883bf14b6592
craftgarrick04401 <craftgarrick04401@gmail.com>
2016-02-22 13:23:09 -0500

    changed crossheirrrrrrrr

--- Dave/src/org/usfirst/frc/team4546/robot/Robot.java
+++ Dave/src/org/usfirst/frc/team4546/robot/Robot.java
@@ -46,10 +46,10 @@ public class Robot extends IterativeRobot {
     final NIVision.Point minVertical = new NIVision.Point(320, 0);
 	final NIVision.Point maxVertical2 = new NIVision.Point(320, 125);
     final NIVision.Point minVertical2 = new NIVision.Point(320, 480);
-    final NIVision.Point maxHorizontal = new NIVision.Point(325, 235);
-    final NIVision.Point minHorizontal = new NIVision.Point(325, 245);
-    final NIVision.Point maxHorizontal2 = new NIVision.Point(325, 245);
-    final NIVision.Point minHorizontal2 = new NIVision.Point(325, 235);
+    final NIVision.Point maxHorizontal = new NIVision.Point(325, 125);
+    final NIVision.Point minHorizontal = new NIVision.Point(325, 115);
+    final NIVision.Point maxHorizontal2 = new NIVision.Point(315, 125);
+    final NIVision.Point minHorizontal2 = new NIVision.Point(315, 115);
 	CameraServer camera;
 	//public static NetworkTable table;
 	
