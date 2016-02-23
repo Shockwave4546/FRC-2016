c247d5af12830480c9d876d69a45d60b3545f3e7
Nolan Greig <ngreig85@comcast.net>
2016-02-23 15:14:22 -0500

    Fleshed out D

--- Dave/src/org/usfirst/frc/team4546/robot/commands/DriveOverObstacle.java
+++ Dave/src/org/usfirst/frc/team4546/robot/commands/DriveOverObstacle.java
@@ -4,6 +4,28 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class DriveOverObstacle extends CommandGroup {
 
+	public enum Obstacle	{
+		
+		RAMPART	(1),
+		MOAT	(1),
+		WALL	(1),
+		TERRAIN	(1);
+		
+		private final double speed;
+		Obstacle(double speed)	{
+			this.speed = speed;
+		}
+		
+		double speed()	{
+			 return speed;
+		}
+	}
+	
+	public DriveOverObstacle(Obstacle obstacle)	{
+		
+		addSequential(new AutoDrive(obstacle.speed(), 59.6));
+	}
+	
 	public DriveOverObstacle()	{
 		
 		addSequential(new AutoDrive(1, 59.6));
